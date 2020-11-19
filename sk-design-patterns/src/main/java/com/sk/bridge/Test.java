a92477ca05b4284f23b5b75cd72dbaec1f901b84
sk <731109627@qq.com>
2020-11-19 12:26:00 +0800

    代理模式

---
+++ sk-design-patterns/src/main/java/com/sk/bridge/Test.java
@@ -0,0 +1,15 @@
+package com.sk.bridge;
+
+/**
+ * TODO
+ *
+ * @author zhangqiao
+ * @since 2020/11/19 12:25
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ sk-design-patterns/src/main/java/com/sk/proxy/Test.java
@@ -0,0 +1,15 @@
+package com.sk.proxy;
+
+/**
+ * TODO
+ *
+ * @author zhangqiao
+ * @since 2020/11/19 7:41
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
